9f47dc5db27030b02df9be5f3d734ac8395898d3
Maksim Logvinenko <mlogvinenko@gmail.com>
2017-03-31 22:21:06 +0300

    Scala 2.11 for samza artifact (#6)

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 scalaVersion := "2.10.5"
 
-crossScalaVersions := Seq("2.10.5", "2.11.7")
+crossScalaVersions := Seq("2.10.6", "2.11.8")
 
 // Disable parallel execution, the various Druid oriented tests need to claim ports
 parallelExecution in ThisBuild := false
@@ -19,7 +19,7 @@ val guiceVersion = "4.0"
 val flinkVersion = "1.0.3"
 val finagleVersion = "6.31.0"
 val twitterUtilVersion = "6.30.0"
-val samzaVersion = "0.8.0"
+val samzaVersion = "0.12.0-mmx3"
 val sparkVersion = "1.6.0"
 val scalatraVersion = "2.3.1"
 val jettyVersion = "9.2.5.v20141112"
@@ -98,7 +98,7 @@ val stormDependencies = Seq(
 )
 
 val samzaDependencies = Seq(
-  "org.apache.samza" % "samza-api" % samzaVersion % "optional"
+  "org.apache.samza" % "samza-api" % samzaVersion % "optional" exclude("org.slf4j", "slf4j-log4j12")
 )
 
 val sparkDependencies = Seq(
@@ -145,11 +145,13 @@ def flinkTestDependencies(scalaVersion: String) = {
     loggingDependencies.map(_ % "test")
 }
 
-// Force 2.10 here, makes update resolution happy, but since w'ere not building for 2.11
-// we won't end up in runtime version hell by doing this.
-val samzaTestDependencies = Seq(
-  "org.apache.samza" % "samza-core_2.10" % samzaVersion % "test"
-)
+val samzaTestDependencies = {
+  Seq(
+    "org.apache.samza" %% "samza-core" % samzaVersion % "test",
+    "org.apache.samza" %% "samza-kafka" % samzaVersion % "test"
+  ).map(_ exclude("log4j", "log4j") exclude("org.slf4j", "slf4j-log4j12") force()) ++
+    loggingDependencies.map(_ % "test")
+}
 
 val serverTestDependencies = Seq(
   "org.scalatra" %% "scalatra-test" % scalatraVersion % "test"
@@ -164,9 +166,9 @@ lazy val commonSettings = Seq(
 
   javaOptions := Seq("-Xms512m", "-Xmx512m", "-XX:MaxPermSize=256M"),
 
-  // Target Java 7
-  scalacOptions += "-target:jvm-1.7",
-  javacOptions in compile ++= Seq("-source", "1.7", "-target", "1.7"),
+  // Target Java 8
+  scalacOptions += "-target:jvm-1.8",
+  javacOptions in compile ++= Seq("-source", "1.8", "-target", "1.8"),
 
   // resolve-term-conflict:object since storm-core has a package and object with the same name
   scalacOptions := Seq("-feature", "-deprecation", "-Yresolve-term-conflict:object"),
@@ -227,10 +229,6 @@ lazy val samza = project.in(file("samza"))
   .settings(commonSettings: _*)
   .settings(name := "tranquility-samza")
   .settings(libraryDependencies ++= (samzaDependencies ++ samzaTestDependencies))
-  // don't compile or publish for Scala > 2.10
-  .settings((skip in compile) := scalaVersion { sv => !sv.startsWith("2.10.") }.value)
-  .settings((skip in test) := scalaVersion { sv => !sv.startsWith("2.10.") }.value)
-  .settings(publishArtifact <<= scalaVersion { sv => sv.startsWith("2.10.") })
   .settings(publishArtifact in Test := false)
   .dependsOn(core % "test->test;compile->compile")
 
--- samza/src/test/scala/com/metamx/tranquility/test/SamzaDruidTest.scala
+++ samza/src/test/scala/com/metamx/tranquility/test/SamzaDruidTest.scala
@@ -22,7 +22,8 @@ import com.metamx.common.scala.Logging
 import com.metamx.common.scala.Predef._
 import com.metamx.common.scala.timekeeper.TestingTimekeeper
 import com.metamx.tranquility.beam.Beam
-import com.metamx.tranquility.samza.BeamFactory
+import com.metamx.tranquility.samza.{BeamFactory, BeamSystemFactory}
+import com.metamx.tranquility.test.SamzaDruidCoordinatorSystemFactory.ssp
 import com.metamx.tranquility.test.common.CuratorRequiringSuite
 import com.metamx.tranquility.test.common.DruidIntegrationSuite
 import com.metamx.tranquility.test.common.JulUtils
@@ -34,11 +35,7 @@ import org.apache.samza.config.MapConfig
 import org.apache.samza.job.JobRunner
 import org.apache.samza.job.local.ThreadJobFactory
 import org.apache.samza.metrics.MetricsRegistry
-import org.apache.samza.system.IncomingMessageEnvelope
-import org.apache.samza.system.OutgoingMessageEnvelope
-import org.apache.samza.system.SystemFactory
-import org.apache.samza.system.SystemStream
-import org.apache.samza.system.SystemStreamPartition
+import org.apache.samza.system._
 import org.apache.samza.task.MessageCollector
 import org.apache.samza.task.StreamTask
 import org.apache.samza.task.TaskCoordinator
@@ -49,6 +46,7 @@ import org.scala_tools.time.Imports._
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import scala.collection.JavaConverters._
+import scala.collection.mutable.ListBuffer
 
 class SamzaDruidTestBeamFactory extends BeamFactory
 {
@@ -98,6 +96,7 @@ class SamzaDruidTestInputSystemFactory extends SystemFactory
         val incoming = DirectDruidTest.generateEvents(now).zipWithIndex map { case (message, index) =>
           new IncomingMessageEnvelope(ssp, index.toString, null, message)
         }
+        register(ssp, null)
         putAll(ssp, incoming.asJava)
       }
 
@@ -111,6 +110,70 @@ class SamzaDruidTestInputSystemFactory extends SystemFactory
 
   override def getAdmin(systemName: String, config: Config) = {
     new SinglePartitionWithoutOffsetsSystemAdmin
+  }
+}
+
+class SamzaDruidCoordinatorSystemFactory extends SystemFactory
+{
+  override def getConsumer(systemName: String, config: Config, registry: MetricsRegistry) = {
+    new BlockingEnvelopeMap()
+    {
+      override def start() {
+        register(ssp, null)
+        putAll(ssp, SamzaDruidCoordinatorSystemFactory.messages.asJava)
+        setIsAtHead(ssp, true)
+      }
+
+      override def stop() {}
+    }
+  }
+
+  override def getProducer(systemName: String, config: Config, registry: MetricsRegistry) = {
+    new SystemProducer {
+      var index = 0
+
+      override def start(): Unit = {}
+
+      override def stop(): Unit = {}
+
+      override def flush(s: String): Unit = {}
+
+      override def register(s: String): Unit = {}
+
+      override def send(s: String, envelope: OutgoingMessageEnvelope): Unit = {
+        SamzaDruidCoordinatorSystemFactory.messages += new IncomingMessageEnvelope(
+          SamzaDruidCoordinatorSystemFactory.ssp, index.toString, envelope.getKey, envelope.getMessage
+        )
+        index += 1
+      }
+    }
+  }
+
+  override def getAdmin(systemName: String, config: Config) = {
+    new SinglePartitionWithoutOffsetsSystemAdmin {
+      override def createCoordinatorStream(streamName: String): Unit = {}
+    }
+  }
+}
+
+object SamzaDruidCoordinatorSystemFactory {
+  private val ssp = new SystemStreamPartition("dummycoordinator", "__samza_coordinator_dummy_1", new Partition(0))
+
+  private val messages = ListBuffer[IncomingMessageEnvelope]()
+
+  private val consumer = new BlockingEnvelopeMap()
+  {
+    override def start() {
+      register(ssp, null)
+      setIsAtHead(ssp, true)
+    }
+
+    override def stop() {}
+
+    def put(incoming: IncomingMessageEnvelope): Unit = {
+      register(ssp, null)
+      put(ssp, incoming)
+    }
   }
 }
 
@@ -130,12 +193,14 @@ class SamzaDruidTest
           Map(
             "job.name" -> "dummy",
             "job.factory.class" -> classOf[ThreadJobFactory].getCanonicalName,
+            "job.coordinator.system" -> "dummycoordinator",
             "task.class" -> classOf[SamzaDruidTestTask].getCanonicalName,
             "task.inputs" -> "dummyin.dummy",
             "tranquility.now" -> now.toString(),
             "tranquility.zkConnect" -> zkConnect,
             "systems.dummyin.samza.factory" -> classOf[SamzaDruidTestInputSystemFactory].getCanonicalName,
-            "systems.drood.samza.factory" -> "com.metamx.tranquility.samza.BeamSystemFactory",
+            "systems.dummycoordinator.samza.factory" -> classOf[SamzaDruidCoordinatorSystemFactory].getCanonicalName,
+            "systems.drood.samza.factory" -> classOf[BeamSystemFactory].getCanonicalName,
             "systems.drood.beam.factory" -> classOf[SamzaDruidTestBeamFactory].getCanonicalName,
             "systems.drood.beam.batchSize" -> "1"
           ).asJava
